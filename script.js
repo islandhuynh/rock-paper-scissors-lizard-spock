833f61ae1e5a782e37f9e2e10b1a66ef415eae1b
Island <islandhuynh@gmail.com>
2021-05-04 19:10:45 -0400

    Added functions to check for winner or ties and update the element

--- script.js
+++ script.js
@@ -27,12 +27,78 @@ const choices = {
   spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
 
+let playerScoreNumber = 0;
+let computerScoreNumber = 0;
+let computerChoice = '';
+
 // Reset all 'selected' icons
 const resetSelected = () => allGameIcons.forEach(icon => icon.classList.remove('selected'));
 
+// Random computer choice
+computerRandomChoice = () => {
+  const computerChoiceNumber = Math.random();
+  if (computerChoiceNumber < 0.2) return computerChoice = 'rock';
+  if (computerChoiceNumber < 0.4) return computerChoice = 'paper';
+  if (computerChoiceNumber < 0.6) return computerChoice = 'scissors';
+  if (computerChoiceNumber < 0.8) return computerChoice = 'lizard';
+  return computerChoice = 'spock';
+}
+
+// Add 'selected' styling and computerChoice
+const displayComputerChoice = () => {
+  switch (computerChoice) {
+    case 'rock':
+      computerRock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Rock';
+      break;
+    case 'paper':
+      computerPaper.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Paper';
+      break;
+    case 'scissors':
+      computerScissors.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Scissors';
+      break;
+    case 'lizard':
+      computerLizard.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Lizard';
+      break;
+    case 'spock':
+      computerSpock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Spock';
+      break;
+  }
+};
+
+// Check result, increase scores, update resultText
+const updateScore = (playerChoice) => {
+  if (playerChoice === computerChoice) return resultText.textContent = "It's a Tie!";
+
+  // Checks if the computer's choice was included in the choice's object defeat array
+  const playerWinExist = choices[playerChoice].defeats.filter(winsAgainst => winsAgainst === computerChoice);
+
+  if (playerWinExist.length > 0) {
+    playerScoreNumber++;
+    playerScoreEl.textContent = playerScoreNumber;
+    return resultText.textContent = "You Win!";
+  };
+
+  computerScoreNumber++;
+  computerScoreEl.textContent = computerScoreNumber;
+  return resultText.textContent = "You Lose!"
+}
+
+// Call functions to process the turn
+const checkResult = playerChoice => {
+  resetSelected();
+  computerRandomChoice();
+  displayComputerChoice();
+  updateScore(playerChoice);
+}
+
 // Passing player selection value and styling icons
 const select = playerChoice => {
-  resetSelected();
+  checkResult(playerChoice);
   // Add 'selected' styling and playerChoice
   switch (playerChoice) {
     case 'rock':
